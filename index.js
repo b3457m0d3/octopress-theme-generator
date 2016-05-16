3a44a6750b724a3a8fcc53c352ad55ed0068d76b
Jessie Taylor <b3457xm0d3@gmail.com>
2016-05-16 17:33:46 -0400

    testing
    1,2

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 console.log("fight!");
-var prompt = require('prompt');
-prompt.start();
+var fs = require("fs");
+var prompt = require('minimal-prompt');
 
-  //
-  // Get two properties from the user: username and email
-  //
-  prompt.get(['username', 'email'], function (err, result) {
-    //
-    // Log the results.
-    //
-    console.log('Command-line input received:');
-    console.log('  username: ' + result.username);
-    console.log('  email: ' + result.email);
-  });
+prompt.question(['Theme Name', 'Theme Repo'], {
+    prompt: '>',
+    delimiter: ':',
+    formatPrompt: function(prompt, delim, name) {
+        // Note: this is the default prompt for the question option.
+        return prompt + delim + ' ' + name + delim + ' ';
+    },
+    onComplete: function(results) {
+        console.log('Theme name:', results.themeName);
+        console.log('Theme repo:', results.themeRepo);
+    }
+});
+
+// prompt.start() will begin the prompting process.
+prompt.start();
