ce278ff54d084fc073940b17fc08c4331c450d18
Jessie Taylor <b3457xm0d3@gmail.com>
2016-05-16 17:55:40 -0400

    kl;k;
    lkjlkjl

--- index.js
+++ index.js
@@ -11,12 +11,28 @@ prompt.question(['Theme Name'], {
         return prompt + delim + ' ' + name + delim + ' ';
     },
     onComplete: function(results) {
-        var themeName = results.themeName;
-        fs.mkdir('./'+themeName,0755,function(err){
-           if (err) {
-               return console.error(err);
-           }
-           console.log('Folder "'+themeName+'" created successfully');
+        var name = results.themeName;
+        fs.mkdir('./'+name,0755,function(err){
+           if (err) return console.error(err);
+           console.log('Folder "'+name+'" created successfully');
+           prompt.question(['Theme Name'], {
+               prompt: '>',
+               delimiter: ':',
+               formatPrompt: function(prompt, delim, name) {
+                   // Note: this is the default prompt for the question option.
+                   return prompt + delim + ' ' + name + delim + ' ';
+               },
+               onComplete: function(results) {
+                   var name = results.themeName;
+                   fs.mkdir('./'+name,0755,function(err){
+                      if (err) return console.error(err);
+                      console.log('Folder "'+name+'" created successfully');
+                   });
+               }
+           });
+
+           // prompt.start() will begin the prompting process.
+           prompt.start();
         });
     }
 });
